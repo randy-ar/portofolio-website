ab43498f060777e3fa861370ad7b3968c98db255
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2023-08-06 01:23:35 +0700

    PORTOFOLIO BERES GAN

--- views/admin/porto_folio/index.php
+++ views/admin/porto_folio/index.php
@@ -23,42 +23,36 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
-                <table class="table table-bordered table-responsive table-stripped table-dark table-hover w-100" style="white-space: nowrap;">
-                <tr>
-                    <th width="1%">No</th>
-                    <th>Judul</th>
-                    <th>Deskripsi</th>
-                    <th>image</th>
-                    <th>link</th>
-                </tr>
-                <?php
-                    require_once $_SERVER['DOCUMENT_ROOT'].'/models/porto_folio.php';
-                    $list_porto_folio = porto_folio::all();
-                    $no = 1;
-                    foreach ($list_porto_folio as $porto_folio) {
-                        echo '
-                        <tr>
-                            <td widtd="1%">'.$no.'</td>
-                            <td>'.$porto_folio->judul.'</td>
-                            <td>'.$porto_folio->deskripsi.'</td>
-                            <td><img src="'.$porto_folio->getImage().'" style="width: 100px;"></td>
-                            <td>'.$porto_folio->link.'</td>
-                            <td class="text-center">
-                                <a href="/admin/porto_folio/edit?id='.$porto_folio->id.'" class="btn btn-outline-success">Edit <i class="ri-edit-box-line"></i></a>
-                                <a href="/admin/porto_folio/delete?id='.$porto_folio->id.'" class="btn btn-outline-danger" onclick="return confirm(`Hapus data ini? data yang telah dihapus tidak dapat dikembalikan.`);">Delete <i class="ri-delete-bin-2-line"></i></a>
-                            </td>
-                        </tr>
-                        ';
-                        $no ++;
-                    }
+              <div class="row mt-4 justify-content-center">   
+        <?php
+            require_once $_SERVER['DOCUMENT_ROOT'].'/models/porto_folio.php';
+            $list_porto_folio = porto_folio::all();
+            $no = 1;
+            foreach ($list_porto_folio as $porto_folio) {
+                echo '
+                <div class="col-md-4 mt-3 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="'.$porto_folio->getImage().'" class="card-img-top" alt="...">
+                    <div class="card-body bg-dark">
+                        <h5 class="card-title"><a href="'.$porto_folio->link.'">'.$porto_folio->judul.'</a></h5>
+                        <p class="card-text">'.$porto_folio->deskripsi.'</p>
+                        <a href="/admin/porto_folio/edit?id='.$porto_folio->id.'" class="btn btn-outline-success">Edit <i class="ri-edit-box-line"></i></a>
+                        <a href="/admin/porto_folio/delete?id='.$porto_folio->id.'" class="btn btn-outline-danger" onclick="return confirm(`Hapus data ini? data yang telah dihapus tidak dapat dikembalikan.`);">Delete <i class="ri-delete-bin-2-line"></i></a>
+                    </div>
+                </div>
+                </div>         
+                ';
+                $no ++;
+            }
                 ?>
-              </table>
+        </div>   
+    </div>
                 </div>
               </div>
           </div>
       </div>
-  </div>
+      
+</div>                 
   
   <?php include $_SERVER['DOCUMENT_ROOT'].'/views/layouts/footer.php' ?>
 </body>
